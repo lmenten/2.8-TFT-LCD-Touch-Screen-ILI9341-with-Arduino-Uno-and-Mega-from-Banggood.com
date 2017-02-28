017fffe73a96f6fa4b5e002a3f3d71cc585969b9
lmenten <lmenten@gmail.com>
2017-02-28 07:09:04 -0500

    A port of the bit-race app.  Like crazy taxi.

---
+++ Adafruit_TFTLCD/examples/taxi_dotmatrix/LedSimulation.h
@@ -0,0 +1,21 @@
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+
+class LedSimulation {
+  private:
+    Adafruit_TFTLCD *pTft;
+    unsigned char Scene[16][16];
+    unsigned char SceneWas[16][16];
+  public:
+    LedSimulation::LedSimulation(Adafruit_TFTLCD &tft);
+    void clear(void);
+    void clearScene(void);
+    void display(void);
+    void draw(void);
+    void setScene(unsigned char Scene[16][16]);
+    void setLedOn(int x, int y);
+    void setLedOff(int x, int y);
+    void setLedBlue(int x, int y);
+    void setLedGreen(int x, int y);
+};
